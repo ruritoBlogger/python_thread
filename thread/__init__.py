ab69803942d173572284641583b217a562092d01
rurito0125 <rurito.blog@gmail.com>
2020-10-16 16:46:49 +0900

    同じクラス内で複数のスレッドを建てた時のサンプルを実装

--- thread/__init__.py
+++ thread/__init__.py
@@ -1,13 +1,14 @@
 import time
 
 def run_one_thread():
-    thread_one = Sample_Thread()
+    thread_one = Sample_Threads()
+    # thread_one = Sample_Thread()
     thread_one.start()
     time.sleep(5)
     thread_one.stop()
 
 if __name__ == "__main__":
-    from thread import Sample_Thread
+    from thread import *
     run_one_thread()
 else:
-    from .thread import Sample_Thread
+    from .thread import *
--- thread/thread.py
+++ thread/thread.py
@@ -22,3 +22,37 @@ class Sample_Thread:
 
     def print_thread_info(self, msg: str):
         print("[thread id: {}] {}".format(get_ident(), msg))
+
+class Sample_Threads:
+
+    def __init__(self):
+        self._thread_one = None
+        self._thread_two = None
+        self._flag = True
+        self._data = None
+
+    def start(self):
+        self._thread_one = Thread(target=self.heavy_task)
+        self._thread_two = Thread(target=self.heavy_task)
+        self._thread_one.start()
+        self._thread_two.start()
+
+    def stop(self):
+        self._flag = False
+        self._thread_one.join()
+        self._thread_two.join()
+
+    def heavy_task(self):
+        self.print_thread_info("start")
+        self.set_data(str(get_ident()))
+        self.print_thread_info("data is {}".format(self._data))
+        while self._flag:
+            pass
+        self.print_thread_info("end")
+        self.print_thread_info("data is {}".format(self._data))
+
+    def set_data(self, msg: str):
+        self._data = msg
+
+    def print_thread_info(self, msg: str):
+        print("[thread id: {}] {}".format(get_ident(), msg))
