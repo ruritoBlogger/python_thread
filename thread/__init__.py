fe774d8323c27e7cf4e38afeef7866422c06418d
rurito0125 <rurito.blog@gmail.com>
2020-10-16 16:58:28 +0900

    fixup! 現在動作しているthread数を吐き出す処理を実装

--- thread/__init__.py
+++ thread/__init__.py
@@ -1,14 +1,20 @@
 import time
 
 def run_one_thread():
-    thread_one = Sample_Threads()
-    # thread_one = Sample_Thread()
+    thread_one = Sample_Thread()
     thread_one.start()
     time.sleep(5)
     thread_one.stop()
 
+def run_two_threads():
+    threads = Sample_Threads()
+    threads.start()
+    time.sleep(5)
+    threads.stop()
+
 if __name__ == "__main__":
     from thread import *
     run_one_thread()
+    run_two_threads()
 else:
     from .thread import *
