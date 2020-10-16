67b82e032e301c7894b20faccb887c5b99d71571
rurito0125 <rurito.blog@gmail.com>
2020-10-16 16:28:57 +0900

    threadのサンプルコードの軸を追加

---
+++ thread/thread.py
@@ -0,0 +1,25 @@
+from threading import Thread
+import time
+
+class Sample_Thread:
+
+    def __init__(self):
+        self._thread = None
+        self._flag = True
+
+    def start(self):
+        self._thread = Thread(target=self.heavy_task)
+        self._thread.start()
+
+    def stop(self):
+        self._flag = False
+        self._thread.join()
+
+    def heavy_task(self):
+        self.print_thread_info("start")
+        while self._flag:
+            pass
+        self.print_thread_info("end")
+
+    def print_thread_info(self, msg: str):
+        pass
